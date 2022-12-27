a8b2bdfda4fb5b23aa116a0382e96be769c20556
NDelan <nutakordelanyo2@gmail.com>
2022-12-26 22:38:59 -0500

    Java Data types

---
+++ Intro/Datatypes.java
@@ -0,0 +1,21 @@
+public class Datatypes {
+    /*DATA TYPES */
+    
+    public static void main(String args[])
+    {
+    //Two types of Language: Statically typed and Dynamically typed languages
+
+    //STATICALLY TYPED: Each variable type already known at compile time
+    //Cannot hold other data types once declared
+    //DYNAMICALLY TYPED: Can receive different data types over time
+
+    /*DATA TYPES IN JAVA*/
+    /*
+     1. Primitive Data Type: bool, char, int, short, byte, long, float, and double
+     2. Non-Primitive Data Type:String, Array
+     Syntax: Datatype name;
+    */
+    short aSingleByte = 100;
+
+}
+}
---
+++ Intro/first.java
@@ -0,0 +1,7 @@
+public class first {
+    static int age;
+    public static void main(String[] args) {
+        System.out.println("Hello, World! " + age);
+    }
+}
+
