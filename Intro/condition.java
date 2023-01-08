255726503c8aeb04355f9f24fc3a4a1f2284068f
NDelan <nutakordelanyo2@gmail.com>
2023-01-07 23:32:00 -0500

    Ternary Operators

--- Intro/condition.java
+++ Intro/condition.java
@@ -7,5 +7,10 @@ public class condition {
         } else {
             System.out.println("Boiling");
         }
+        // Using Ternary Operator
+        // Syntax: variable = (condition) ? expressionTrue : expressionFalse;
+
+        String result = (temp < 100) ? "Not Boiling" : "Oops! Boiling.";
+        System.out.println(result);
     }
 }
