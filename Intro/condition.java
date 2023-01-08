85d60d4673c92b11abd317e9fc8c3d4388a9ac5a
NDelan <nutakordelanyo2@gmail.com>
2023-01-07 23:26:11 -0500

    Traditional if-else statement

---
+++ Intro/condition.java
@@ -0,0 +1,11 @@
+public class condition {
+    public static void main(String[] args){
+        // Short Hand If...Else Statement(Ternary Operator)
+        int temp = 102;
+        if (temp < 100){
+            System.out.println("Not Boiling");
+        } else {
+            System.out.println("Boiling");
+        }
+    }
+}
