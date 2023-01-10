80a068aa7f7ce3d7c028ac98a7ba5d8598b02c8e
NDelan <nutakordelanyo2@gmail.com>
2023-01-09 20:33:46 -0500

    Method definition and call

---
+++ Intro/method.java
@@ -0,0 +1,11 @@
+public class method {
+    //Define method
+    static void myMethod(String fname, int age){
+        System.out.println("Legooo");
+        System.out.println("I am " + fname + ". I am " + age);
+    }
+    //Call method
+    public static void main(String[] args){
+        myMethod("Del", 12);
+    }
+}
