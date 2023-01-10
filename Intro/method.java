e475249fd46dd7219b3b02b549990d0204fbabc5
NDelan <nutakordelanyo2@gmail.com>
2023-01-09 20:49:28 -0500

    Using if-else in methods

--- Intro/method.java
+++ Intro/method.java
@@ -4,8 +4,17 @@ public class method {
         System.out.println("Legooo");
         System.out.println("I am " + fname + ". I am " + age);
     }
+
+    static void myAge(int age){
+        if (age < 18 ) {
+            System.out.println("Under age");
+        } else {
+            System.out.println("Adult");
+        }
+    }
     //Call method
     public static void main(String[] args){
         myMethod("Del", 12);
+        // myAge(12);
     }
 }
