87b9b67ececc74042bd7adf29e0c2b0bd79e65c5
NDelan <nutakordelanyo2@gmail.com>
2023-01-11 20:37:42 -0500

    Class objects and attributes

---
+++ Intro/oop.java
@@ -0,0 +1,12 @@
+public class oop {
+    int x = 5;
+    public static void main(String[] args) {
+      oop myObj = new oop();
+      System.out.println(myObj.x);
+    //   set new value
+    myObj.x = 40;
+    System.out.println(myObj.x);
+
+    // Use 'final' to set the value to constant 
+    }
+}
