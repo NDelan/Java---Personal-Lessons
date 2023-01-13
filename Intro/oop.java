e3a50bfcbe8747ef3573ff679e551cdfe1741264
NDelan <nutakordelanyo2@gmail.com>
2023-01-12 22:12:01 -0500

    Public and Static Methods

--- Intro/oop.java
+++ Intro/oop.java
@@ -1,12 +1,21 @@
 public class oop {
     int x = 5;
     public static void main(String[] args) {
-      oop myObj = new oop();
-      System.out.println(myObj.x);
-    //   set new value
-    myObj.x = 40;
-    System.out.println(myObj.x);
+      oop myObj1 = new oop();
+      oop myObj2 = new oop();
 
-    // Use 'final' to set the value to constant 
+      myObj2.x = 89;
+      System.out.println(myObj1.x);
+    //   set new value
+      // myObj.x = 40;
+      System.out.println(myObj2.x);
     }
+
+      // Use 'final' to set the value to constant 
+
+    // Public
+    public void myPublicMethod() {
+      System.out.println("Public method");
+    }
+
 }
