4df94f4b07469f2b5d849794fe7dd8268910e6c5
NDelan <nutakordelanyo2@gmail.com>
2023-01-15 17:36:24 -0500

    Constructors & Constructor Parameters

--- Intermediate/FIRSTCLASSPROJECT/src/Vehicle.java
+++ Intermediate/FIRSTCLASSPROJECT/src/Vehicle.java
@@ -9,13 +9,31 @@ public class Vehicle {
     void horn() {
         System.out.println("Beep!");
     }
+
+    // Constructor
+    public Vehicle(){
+        wheels = 5;
+    }
+
+    // Pass parameters to constructors
+    public Vehicle(int y){
+        maxSpeed = y;
+    }
+
 }
 
 class MyClass {
+    // Accessing attributes
     public static void main(String[ ] args) {
         Vehicle v1 = new Vehicle();
         Vehicle v2 = new Vehicle();
+
+        Vehicle v3 = new Vehicle(454);
+
         v1.color = "red";
         v2.horn();
+        System.out.println(v2.wheels);
+
+        System.out.println(v3.maxSpeed);
     }
 }
