d4b98dadc7f72e8c9ef2801a3a7ff31139308490
NDelan <nutakordelanyo2@gmail.com>
2023-01-15 16:44:34 -0500

    Defining Attributes

---
+++ Intermediate/FIRSTCLASSPROJECT/src/Vehicle.java
@@ -0,0 +1,13 @@
+public class Vehicle {
+    // Class Attributes Definition
+    int maxSpeed;
+    int wheels;
+    String color;
+    double fuelCapacity;
+
+    // Methods
+    void horn() {
+        System.out.println("Beep!");
+    }
+}
+
