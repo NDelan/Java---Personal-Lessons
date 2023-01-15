67cfb831df6f9ccac8123afdd85e4410a5f962b4
NDelan <nutakordelanyo2@gmail.com>
2023-01-15 16:46:26 -0500

    Creating objects; accessing attributes & methods

--- Intermediate/FIRSTCLASSPROJECT/src/Vehicle.java
+++ Intermediate/FIRSTCLASSPROJECT/src/Vehicle.java
@@ -11,3 +11,11 @@ public class Vehicle {
     }
 }
 
+class MyClass {
+    public static void main(String[ ] args) {
+        Vehicle v1 = new Vehicle();
+        Vehicle v2 = new Vehicle();
+        v1.color = "red";
+        v2.horn();
+    }
+}
