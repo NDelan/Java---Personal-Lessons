ded30d5436dffa3258bc821577a1fe53e5396416
NDelan <nutakordelanyo2@gmail.com>
2023-01-17 22:01:01 -0500

    Inheritance: Subclass and Superclass

---
+++ Intermediate/FIRSTCLASSPROJECT/src/Inheritance.java
@@ -0,0 +1,16 @@
+public class Inheritance {
+    protected String Ex1 = "private Example1";
+    public void publicMethod(){
+        System.out.println("This is public");
+    }
+}
+class Subclass extends Inheritance {
+    //private String subAttribute = "Attribute of subclass";
+    public static void main(String[] args) {
+
+        //New obj
+        Subclass myObj = new Subclass();
+        //Call method from super class
+        myObj.publicMethod();
+    }
+}
