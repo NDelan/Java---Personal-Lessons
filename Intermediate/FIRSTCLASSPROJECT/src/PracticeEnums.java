ede18b8c8b3338a0e945c29bbe8b5e82e31351a2
NDelan <nutakordelanyo2@gmail.com>
2023-01-22 19:11:37 -0500

    Enums in class

--- Intermediate/FIRSTCLASSPROJECT/src/PracticeEnums.java
+++ Intermediate/FIRSTCLASSPROJECT/src/PracticeEnums.java
@@ -1,12 +1,48 @@
-enum Level {
-    LOW, MEDIUM, HIGH;
-}
+// Enums outside a class
+
+// enum Level {
+//     LOW, MEDIUM, HIGH;
+// }
+
+// public class PracticeEnums {
+//     // static String[] levels = {"Low", "Medium", "High"};
+//     public static void main(String[] args){
+//         Level l = Level.LOW;
+//         System.out.println(l);
+
+//         switch (l) {
+//             case LOW:
+//             System.out.println("Low Level");
+//             break;
+//             case MEDIUM:
+//             System.out.println("Low Level");
+//             break;
+//             case HIGH:
+//             System.out.println("Low Level");
+//             break;          
+//         }
+//     }
+// }
+
+// Enums inside a class
+
 
 public class PracticeEnums {
     // static String[] levels = {"Low", "Medium", "High"};
+
+    enum Level {
+        LOW, MEDIUM, HIGH;
+
+        public static void getLevel() {
+            System.out.println(Level.LOW);
+        }
+    }
     public static void main(String[] args){
         Level l = Level.LOW;
         System.out.println(l);
+
+        // Call Method
+        Level.getLevel();
 
         switch (l) {
             case LOW:
