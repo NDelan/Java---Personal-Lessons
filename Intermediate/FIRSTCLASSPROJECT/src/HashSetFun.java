ef5205f3e95a5a2b31755d9b2016765b820d2e5e
NDelan <nutakordelanyo2@gmail.com>
2023-01-25 22:57:33 -0500

    Hashsets

---
+++ Intermediate/FIRSTCLASSPROJECT/src/HashSetFun.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+
+public class HashSetFun {
+    public static void main(String[] args){
+        HashSet <String> setObj = new HashSet<String>();
+        setObj.add("Lau");
+        setObj.add("Del");
+        
+        System.out.println(setObj);
+    }
+
+}
+
