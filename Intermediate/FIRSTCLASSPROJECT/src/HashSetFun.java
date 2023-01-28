bb4b00b179b484e5c068dee55a9b34e972988ff3
NDelan <nutakordelanyo2@gmail.com>
2023-01-27 20:14:18 -0500

    Create ArrayList

--- Intermediate/FIRSTCLASSPROJECT/src/HashSetFun.java
+++ Intermediate/FIRSTCLASSPROJECT/src/HashSetFun.java
@@ -3,11 +3,13 @@ import java.util.HashSet;
 public class HashSetFun {
     public static void main(String[] args){
         HashSet <String> setObj = new HashSet<String>();
+        setObj.add("Hap");
         setObj.add("Lau");
         setObj.add("Del");
-        
+
+        for (String i: setObj){
+            System.out.println(i);
+        }
         System.out.println(setObj);
     }
-
 }
-
---
+++ Intermediate/FIRSTCLASSPROJECT/src/IteratorFun.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class IteratorFun {
+    public static void main(String args[]) {
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("Del");
+        names.add("Lau");
+        names.add("Vhim");
+
+    }
+}
