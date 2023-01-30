d8820cad46e66b79a79025d1f7fdb188e5620d70
NDelan <nutakordelanyo2@gmail.com>
2023-01-29 21:26:44 -0500

    Create Wrapper Classes

---
+++ Intermediate/FIRSTCLASSPROJECT/src/WrapperClassesFun.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class WrapperClassesFun {
+    public static void main(String args[]){
+        // Provides a way to use primitive data types as objects
+        // Use Integer not int
+
+        //ArrayList<Integer> myNumbers = new ArrayList<Integer>();
+
+        Integer myInt = 5;
+        Double myDouble = 5.4;
+        System.out.println(myInt.intValue());
+        System.out.println(myDouble.doubleValue());
+    }
+}
